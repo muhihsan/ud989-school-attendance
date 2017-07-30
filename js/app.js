d5cfc92b06fa3342fafbd1075775b99614ce9002
Muhammad Ihsan <mihsan191@gmail.com>
2017-07-30 20:45:56 +1000

    Store current value in localstorage

--- js/app.js
+++ js/app.js
@@ -25,11 +25,19 @@
 
         init: function() {
             var self = this;
-            self.students.push(new Student('Slappy the Frog', numOfClass));
-            self.students.push(new Student('Lilly the Lizard', numOfClass));
-            self.students.push(new Student('Paulrus the Walrus', numOfClass));
-            self.students.push(new Student('Gregory the Goat', numOfClass));
-            self.students.push(new Student('Adam the Anaconda', numOfClass));
+
+            if (!localStorage.attendance) {
+                console.log('Creating attendance records...');
+                self.students.push(new Student('Slappy the Frog', numOfClass));
+                self.students.push(new Student('Lilly the Lizard', numOfClass));
+                self.students.push(new Student('Paulrus the Walrus', numOfClass));
+                self.students.push(new Student('Gregory the Goat', numOfClass));
+                self.students.push(new Student('Adam the Anaconda', numOfClass));
+                localStorage.attendance = JSON.stringify(self.students);
+            }
+            else {
+                self.students = JSON.parse(localStorage.attendance);
+            }
         }
     }
 
@@ -48,7 +56,9 @@
         },
 
         updateStudentAttendance: function(student, attendanceId) {
+            var self = this;
             student.attendanceDay[attendanceId] = !student.attendanceDay[attendanceId];
+            localStorage.attendance = JSON.stringify(studentModel.students);
         },
 
         countMissedDay: function(student) {
@@ -135,4 +145,79 @@
     };
 
     octopus.init();
+
+    // if (!localStorage.attendance) {
+    //     console.log('Creating attendance records...');
+
+    //     var nameColumns = $('tbody .name-col'),
+    //         attendance = {};
+
+    //     nameColumns.each(function() {
+    //         var name = this.innerText;
+    //         attendance[name] = [];
+
+    //         for (var i = 0; i <= 11; i++) {
+    //             attendance[name].push(getRandom());
+    //         }
+    //     });
+
+    //     localStorage.attendance = JSON.stringify(attendance);
+    // }
 }());
+
+
+/* STUDENT APPLICATION */
+// $(function() {
+//     var attendance = JSON.parse(localStorage.attendance),
+//         $allMissed = $('tbody .missed-col'),
+//         $allCheckboxes = $('tbody input');
+
+//     // Count a student's missed days
+//     function countMissing() {
+//         $allMissed.each(function() {
+//             var studentRow = $(this).parent('tr'),
+//                 dayChecks = $(studentRow).children('td').children('input'),
+//                 numMissed = 0;
+
+//             dayChecks.each(function() {
+//                 if (!$(this).prop('checked')) {
+//                     numMissed++;
+//                 }
+//             });
+
+//             $(this).text(numMissed);
+//         });
+//     }
+
+//     // Check boxes, based on attendace records
+//     $.each(attendance, function(name, days) {
+//         var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
+//             dayChecks = $(studentRow).children('.attend-col').children('input');
+
+//         dayChecks.each(function(i) {
+//             $(this).prop('checked', days[i]);
+//         });
+//     });
+
+//     // When a checkbox is clicked, update localStorage
+//     $allCheckboxes.on('click', function() {
+//         var studentRows = $('tbody .student'),
+//             newAttendance = {};
+
+//         studentRows.each(function() {
+//             var name = $(this).children('.name-col').text(),
+//                 $allCheckboxes = $(this).children('td').children('input');
+
+//             newAttendance[name] = [];
+
+//             $allCheckboxes.each(function() {
+//                 newAttendance[name].push($(this).prop('checked'));
+//             });
+//         });
+
+//         countMissing();
+//         localStorage.attendance = JSON.stringify(newAttendance);
+//     });
+
+//     countMissing();
+// }());
